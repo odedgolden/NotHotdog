47130d4eec0f4035d16a384477a2e6294ba2ef38
Oded Golden <odedgolden@gmail.com>
2017-10-15 16:00:07 +0300

    Initial Commit

---
+++ NotHotdog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotHotdog
+//
+//  Created by Oded Golden on 15/10/2017.
+//  Copyright © 2017 Oded Golden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
