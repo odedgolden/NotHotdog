5deaca6796823f5aa412793b6cf6dff61f4bab84
Oded Golden <odedgolden@gmail.com>
2017-10-18 17:30:46 +0300

    Added models - Resnet50 and VGG16

---
+++ NotHotdog/NHImageTakingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NHImageTakingViewController.swift
+//  NotHotdog
+//
+//  Created by Oded Golden on 18/10/2017.
+//  Copyright © 2017 Oded Golden. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class NHImageTakingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
